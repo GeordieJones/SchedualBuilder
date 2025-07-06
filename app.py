218f63496455e00f2b8b4208abffc256590e31b2
Geordie Jones <geordiejonesnufc@icloud.com>
2025-07-06 10:38:04 -0400

    3 stage input

--- app.py
+++ app.py
@@ -10,10 +10,26 @@ class mainWindow(QWidget):
 
         layout = QVBoxLayout()
 
-        self.label = QLabel('Eneter class: ')
-        layout.addWidget(self.label)
-        self.input_box = QLineEdit()
-        layout.addWidget(self.input_box)
+        #classes enter
+        self.label1 = QLabel('Class: ')
+        layout.addWidget(self.label1)
+        self.input_box1 = QLineEdit()
+        layout.addWidget(self.input_box1)
+
+        #days enter
+        self.label2 = QLabel('Days (m, t, w, th, f): ')
+        layout.addWidget(self.label2)
+        self.input_box2 = QLineEdit()
+        layout.addWidget(self.input_box2)
+
+        #times enter
+        self.label3 = QLabel('time (start-finish): ')
+        layout.addWidget(self.label3)
+        self.input_box3 = QLineEdit()
+        layout.addWidget(self.input_box3)
+
+
+
         self.submit_button = QPushButton("Submit")
         self.submit_button.clicked.connect(self.process_input)
         layout.addWidget(self.submit_button)
@@ -21,8 +37,13 @@ class mainWindow(QWidget):
         self.setLayout(layout)
 
     def process_input(self):
-        user_input = self.input_box.text()
-        print(f'Input: {user_input}')
+        class_name = self.input_box1.text()
+        days = self.input_box2.text()
+        time_range = self.input_box3.text()
+
+        print(f"Class: {class_name}")
+        print(f"Days: {days}")
+        print(f"Time: {time_range}")
 
 
 def main():
