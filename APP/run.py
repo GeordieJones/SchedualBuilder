3cb9a8606bb93c2ac5d08962a149689e7f9d7e01
Geordie Jones <geordiejonesnufc@icloud.com>
2025-07-09 21:00:58 -0400

    renamed

--- APP/run.py
+++ APP/run.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QApplication
-from new_attempt.windows.input_windows import run as run_main_window
+from APP.windows.input_windows import run as run_main_window
 
 def run_app():
     run_main_window()
